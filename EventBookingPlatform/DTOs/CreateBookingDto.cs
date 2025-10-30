1822239e51383315c3c27e4b65eb943c0136395f
maria vassilikopoulou <maria.vassilikopoulou@iths.se>
2025-10-30 17:01:24 +0100

    corrections on the function app sendbooking email!

--- EventBookingPlatform/DTOs/CreateBookingDto.cs
+++ EventBookingPlatform/DTOs/CreateBookingDto.cs
@@ -7,6 +7,7 @@
         //public string UserEmail {  get; set; } = string.Empty;
         public string EventId { get; set; }= string.Empty;
         public int Seats { get; set; }
+        public string EventName { get; set; } = string.Empty;
 
     }
 }
