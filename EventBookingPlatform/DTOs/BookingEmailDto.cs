de744f18c928409fe469130561db1145728c8bf1
maria vassilikopoulou <maria.vassilikopoulou@iths.se>
2025-10-30 11:59:37 +0100

    implement function app creating domain name to send confirmation booking emails correctly!

---
+++ EventBookingPlatform/DTOs/BookingEmailDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EventBookingPlatform.DTOs
+{
+    public class BookingEmailDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public int Seats { get; set; }
+    }
+}
