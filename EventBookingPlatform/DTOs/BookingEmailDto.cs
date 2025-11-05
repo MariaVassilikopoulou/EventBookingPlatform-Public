f77bb824e77502224660acae5990b4c4d9f0bb59
maria vassilikopoulou <maria.vassilikopoulou@iths.se>
2025-11-05 12:22:08 +0100

    adde corect sql admin and corrections for function upp

--- EventBookingPlatform/DTOs/BookingEmailDto.cs
+++ EventBookingPlatform/DTOs/BookingEmailDto.cs
@@ -2,6 +2,9 @@
 {
     public class BookingEmailDto
     {
+
+        public string EventId { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
         public string UserName { get; set; } = string.Empty;
         public string UserEmail { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
